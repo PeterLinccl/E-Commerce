77f780f32285884cc60c674106432d20640b62e2
Peter Lin <47198276+peterlin456@users.noreply.github.com>
2022-10-09 22:39:22 -0400

    update

--- Spring_Boot_Backend/spring-boot-ecommerce/src/main/java/com/eccommerce/springboot/dao/ProductRepository.java
+++ Spring_Boot_Backend/spring-boot-ecommerce/src/main/java/com/eccommerce/springboot/dao/ProductRepository.java
@@ -12,4 +12,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable page);
+
+
 }
