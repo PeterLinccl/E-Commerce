3f118ef4472b23a1edb755dc5a70360165775766
Peter Lin <47198276+peterlin456@users.noreply.github.com>
2022-10-27 23:07:05 -0400

    backend update

--- Spring_Boot_Backend/spring-boot-ecommerce/src/main/java/com/eccommerce/springboot/dto/PurchaseResponse.java
+++ Spring_Boot_Backend/spring-boot-ecommerce/src/main/java/com/eccommerce/springboot/dto/PurchaseResponse.java
@@ -2,11 +2,12 @@ package com.eccommerce.springboot.dto;
 
 
 import lombok.Data;
+import lombok.NonNull;
 
 //use this to send back a java object as json
 @Data
 public class PurchaseResponse {
-
+    @NonNull
     private String orderTrackingNumber;
 
 
--- Spring_Boot_Backend/spring-boot-ecommerce/src/main/java/com/eccommerce/springboot/service/CheckoutServiceImpl.java
+++ Spring_Boot_Backend/spring-boot-ecommerce/src/main/java/com/eccommerce/springboot/service/CheckoutServiceImpl.java
@@ -4,8 +4,15 @@ package com.eccommerce.springboot.service;
 import com.eccommerce.springboot.dao.CustomerRepository;
 import com.eccommerce.springboot.dto.Purchase;
 import com.eccommerce.springboot.dto.PurchaseResponse;
+import com.eccommerce.springboot.entity.Customer;
+import com.eccommerce.springboot.entity.Order;
+import com.eccommerce.springboot.entity.OrderItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService{
@@ -18,7 +25,39 @@ public class CheckoutServiceImpl implements CheckoutService{
     }
 
     @Override
+    @Transactional
     public PurchaseResponse placeOrder(Purchase purchase) {
-        return null;
+
+        //retrieve the order info from dto
+        Order order = purchase.getOrder();
+
+        //generate tracking  number
+        String orderTrackNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackNumber);
+
+        //populate order with orderItems
+        Set<OrderItem> orderItems = purchase.getOrderItem();
+        for (OrderItem item: orderItems) {
+            order.add(item);
+        }
+        //populate order with billingAddress and shippingAddress
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
+
+        //populate customer with order
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
+
+        //save to the database
+        customerRepository.save(customer);
+
+        //return a response
+        return new PurchaseResponse(orderTrackNumber);
+    }
+
+    //create unique id
+    private String generateOrderTrackingNumber() {
+        //random UUID number
+        return UUID.randomUUID().toString();
     }
 }
