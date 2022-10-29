04c4d76da369e8c3b7b467433e09609aed5d63fe
Peter Lin <47198276+peterlin456@users.noreply.github.com>
2022-10-28 23:26:37 -0400

    update

---
+++ Spring_Boot_Backend/spring-boot-ecommerce/src/main/java/com/eccommerce/springboot/controller/CheckoutController.java
@@ -0,0 +1,29 @@
+package com.eccommerce.springboot.controller;
+
+import com.eccommerce.springboot.dto.Purchase;
+import com.eccommerce.springboot.dto.PurchaseResponse;
+import com.eccommerce.springboot.service.CheckoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    @Autowired
+    public CheckoutController(CheckoutService checkoutService){
+            this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase){
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+
+
+}
