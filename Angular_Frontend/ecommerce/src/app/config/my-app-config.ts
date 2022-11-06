52c8ffe04a26d9fcd6ee1a2f7cedc55f3b237f5c
Peter Lin <47198276+peterlin456@users.noreply.github.com>
2022-11-06 14:22:04 -0500

    okt signin

--- Angular_Frontend/ecommerce/src/app/config/my-app-config.ts
+++ Angular_Frontend/ecommerce/src/app/config/my-app-config.ts
@@ -1,8 +1,8 @@
 export default {
     oidc:{
-        clientId:'',
-        issuer:'',
-        redirectUri: 'http://localhost:4200/login/callback',
-        scopes:['openid','profile','email']
+        clientId:'0oa75e5hnq5YbfuF05d7',//public identifier
+        issuer:'https://dev-33149431.okta.com/oauth2/default',//issue token, url when authorizing with okta Authorization Server
+        redirectUri: 'http://localhost:4200/login/callback',//once user logs in, driect them there
+        scopes:['openid','profile','email']//provide access to information about a user
     }
 }
